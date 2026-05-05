771bd3554b92e739b103a1da621042e7dfcc5397
Hoang-Nguyen-Huy <hoangmegamind1302@gmail.com>
2026-05-05 21:59:49 +0700

    chore(code): fix ci

--- app/[locale]/admin/manage-packages/page.tsx
+++ app/[locale]/admin/manage-packages/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from 'next/navigation';
 
-export default function Page({ params }: { params: { locale: string } }) {
-  redirect(`/${params.locale}/dashboard/manage-packages`);
+export default async function Page({ params }: { params: Promise<{ locale: string }> }) {
+  const { locale } = await params;
+  redirect(`/${locale}/dashboard/manage-packages`);
 }
